aafdbb6ce48ad419ca2b10b3f2f3a331edd9ce00
ethan-walker <101420077+ethan-walker@users.noreply.github.com>
2024-01-03 17:10:01 +0000

    Updated data.js
    + added comments in index.html

--- js/data.js
+++ js/data.js
@@ -3,37 +3,37 @@
 const data = {
 	board : [
 		[
-			{"text" : "civis", "category": 2},
+			{"text" : "credere", "category": 2},
 			{"text" : "quintus", "category": 3},
-			{"text" : "servum", "category": 1},
+			{"text" : "tam", "category": 1},
 			{"text" : "necare", "category": 0}
 		],
 		[
 			{"text" : "horatia", "category": 3},
-			{"text" : "hostis", "category": 2},
-			{"text" : "puerum", "category": 1},
+			{"text" : "persuadere", "category": 2},
+			{"text" : "adeo", "category": 1},
 			{"text" : "occidere", "category": 0}
 		],
 		[
-			{"text" : "senex", "category": 2},
-			{"text" : "agrum", "category": 1},
+			{"text" : "iubere", "category": 2},
+			{"text" : "ita", "category": 1},
 			{"text" : "scintilla", "category": 3},
 			{"text" : "flaccus", "category": 3}
 		],
 		[
 			{"text" : "interficere", "category": 0},
-			{"text" : "pater", "category": 2},
+			{"text" : "favere", "category": 2},
 			{"text" : "extinguere", "category": 0},
-			{"text" : "amicum", "category": 1}
+			{"text" : "tot", "category": 1}
 		]
 	],
 	categories : [
 		{"name" : "To kill"},
-		{"name" : "2nd decl acc sg"},
-		{"name" : "3rd nouns with the same no. syllables in the genitive"},
+		{"name" : "Signpost words"},
+		{"name" : "Verb + dat."},
 		{"name" : "Latin Characters"}
 	],
 	attemptsRemaining: 4,
 	history : [],
-	version : 0
+	version : 999
 }
